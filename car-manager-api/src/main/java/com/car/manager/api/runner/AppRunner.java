5b20e715434b4647fab272f140510d40b404db2f
Danilo de Souza Lima <danilosouza9612@outlook.com>
2025-04-18 20:07:57 -0300

    SonarQube: Remove passwords from application.properties and add test coverage
    Refs: 37

--- car-manager-api/src/main/java/com/car/manager/api/runner/AppRunner.java
+++ car-manager-api/src/main/java/com/car/manager/api/runner/AppRunner.java
@@ -5,6 +5,7 @@ import com.car.manager.repository.schema.UserSchema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -19,17 +20,22 @@ public class AppRunner implements CommandLineRunner {
 
     private final PasswordEncoder passwordEncoder;
 
+    private final String rootPassword;
+
     @Autowired
-    public AppRunner(UserRepository userRepository, PasswordEncoder passwordEncoder){
+    public AppRunner(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder,
+            @Value("${com.car.manager.root.password}") String rootPassword
+    ){
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.rootPassword = rootPassword;
     }
 
     @Override
     public void run(String... args) throws Exception {
         if(userRepository.existsByLogin("root")) return;
-        String login = "root";
-        String password = "123456";
 
         UserSchema userSchema = new UserSchema();
         userSchema.setFirstName("root");
@@ -38,10 +44,10 @@ public class AppRunner implements CommandLineRunner {
         userSchema.setEmail("root@mail.com");
         userSchema.setLogin("root");
         userSchema.setBirthday(LocalDate.now());
-        userSchema.setPassword(passwordEncoder.encode(password));
+        userSchema.setPassword(passwordEncoder.encode(rootPassword));
 
         userRepository.saveAndFlush(userSchema);
 
-        logger.info("User added. Login: {}, Password: {}", login, password);
+        logger.info("User added");
     }
 }
---
+++ car-manager-core/src/main/java/com/car/manager/core/analytics/AnalyticsService.java
@@ -0,0 +1,7 @@
+package com.car.manager.core.analytics;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnalyticsService {
+}
